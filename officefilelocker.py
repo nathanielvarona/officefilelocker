e7ba0f64a3b1b2935ce22f4121ea065f6538b8e1
Nathaniel Varona <nathaniel.varona@gmail.com>
2014-07-07 20:09:36 +0800

    Added Support for Classic XLS File

--- officefilelocker.py
+++ officefilelocker.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env jython
 # This requires Jython and Apache POI
-import os, sys, getopt, ConfigParser
+import os, sys, getopt, ConfigParser, mimetypes
 
 try:
     config = ConfigParser.ConfigParser()
@@ -15,26 +15,38 @@ for root, dirs, files in os.walk(APACHE_POI_JAR_PATH):
             sys.path.append(os.path.join(root, name))
 
 from org.apache.poi.xssf.usermodel import *
+from org.apache.poi.hssf.usermodel import *
 from java.io import FileInputStream
 from java.io import FileOutputStream
 
+xssf_supported_mimetypes = [
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+]
+
+hssf_supported_mimetypes = [
+    "application/vnd.ms-excel"
+]
 
 def main(argv):
     password = ''
     input_file = ''
     output_file = ''
 
-    def message():
+    def get_mimetype(input_file):
+        file_mimetype, file_mime_encoding = mimetypes.guess_type(input_file)
+        return file_mimetype, file_mime_encoding
+
+    def usage():
         print '\nUsage: jython %s -p <password> -i <inputfile> -o <outputfile>\n' % sys.argv[0]
         sys.exit()
 
     try:
         opts, args = getopt.getopt(argv,"hp:i:o:",["password=","input=","output="])
     except getopt.GetoptError:
-        message()
+        usage()
     for opt, arg in opts:
         if opt == '-h':
-            message()
+            usage()
         elif opt in ("-p", "--password"):
             password = arg
         elif opt in ("-i", "--input"):
@@ -43,30 +55,36 @@ def main(argv):
             output_file = arg
 
     if password and input_file and output_file:
+        mimetype = get_mimetype(input_file)[0]
+        # print "Office File MimeType: %s" % mimetype
         fileIn = FileInputStream(input_file)
         fileOut = FileOutputStream(output_file)
 
-        workbook = XSSFWorkbook(fileIn)
-        worksheets = workbook.getNumberOfSheets()
+        if mimetype in xssf_supported_mimetypes:
+            workbook = XSSFWorkbook(fileIn)
+            worksheets = workbook.getNumberOfSheets()
+            for worksheet in range(0,worksheets):
+                sheet = workbook.getSheetAt(worksheet)
+                sheet.lockDeleteColumns()
+                sheet.lockDeleteRows()
+                sheet.lockFormatCells()
+                sheet.lockFormatColumns()
+                sheet.lockFormatRows()
+                sheet.lockInsertColumns()
+                sheet.lockInsertRows()
+                sheet.protectSheet(password)
+                sheet.enableLocking();
+            workbook.lockStructure();
 
-        for worksheet in range(0,worksheets):
-            sheet = workbook.getSheetAt(worksheet)
-            # sheet.sheetName.title()
-            sheet.lockDeleteColumns()
-            sheet.lockDeleteRows()
-            sheet.lockFormatCells()
-            sheet.lockFormatColumns()
-            sheet.lockFormatRows()
-            sheet.lockInsertColumns()
-            sheet.lockInsertRows()
-            sheet.protectSheet(password)
-            sheet.enableLocking();
+        elif mimetype in hssf_supported_mimetypes:
+            workbook = HSSFWorkbook(fileIn)
+            workbook.writeProtectWorkbook(password, password)
 
-        workbook.lockStructure();
         workbook.write(fileOut)
         fileIn.close()
+
     else:
-        message()
+        usage()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
